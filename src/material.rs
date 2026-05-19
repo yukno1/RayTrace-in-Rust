135e566be4f758ce2fd1fedb16bf44c55ac514c8
Yuk1no <121278340+yukno1@users.noreply.github.com>
2026-05-19 14:57:53 +0800

    Schlick approx

--- src/material.rs
+++ src/material.rs
@@ -1,4 +1,4 @@
-use crate::{color::Color, hittable::HitRecord, ray::Ray, vec3::Vec3};
+use crate::{color::Color, hittable::HitRecord, ray::Ray, utils::rand_f64, vec3::Vec3};
 
 pub trait Material {
     fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<(Color, Ray)>; // (attenuation, scattered)
@@ -58,12 +58,20 @@ impl Material for Metal {
 }
 
 pub struct Dielectric {
+    // Refractive index in vaccum or ait, or the ratio of the material's refractive index over the refractive index of enclosing media
     refraction_idx: f64,
 }
 
 impl Dielectric {
     pub fn new(refraction_idx: f64) -> Self {
         Self { refraction_idx }
+    }
+
+    fn reflectance(&self, cos: f64, ri: f64) -> f64 {
+        // use Schlick's approximation for reflectance
+        let mut r0 = (1.0 - ri) / (1.0 + ri);
+        r0 = r0 * r0;
+        r0 + (1.0 - r0) * (1.0 - cos).powi(5)
     }
 }
 
@@ -80,7 +88,7 @@ impl Material for Dielectric {
         let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
 
         let cannot_refract = ri * sin_theta > 1.0;
-        let direction = if cannot_refract {
+        let direction = if cannot_refract || (self.reflectance(cos_theta, ri) > rand_f64()) {
             r_in.direction.reflect(rec.normal)
         } else {
             r_in.direction.refract(rec.normal, ri)
