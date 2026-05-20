1deba8f754783d321a8caeb2f9ab44384a2bc7b2
Yuk1no <121278340+yukno1@users.noreply.github.com>
2026-05-20 16:44:24 +0800

    sphere texture coordinates

--- src/sphere.rs
+++ src/sphere.rs
@@ -1,3 +1,5 @@
+use std::f64::consts::PI;
+
 use crate::{
     aabb::AABB,
     hittable::{HitRecord, Hittable},
@@ -42,6 +44,19 @@ impl<'a> Sphere<'a> {
             bbox: AABB::from_boxes(box1, box2),
         }
     }
+
+    fn get_sphere_uv(p: Point3) -> (f64, f64) {
+        // p: a given point on the sphere of radius one, centered at the origin.
+        // u: returned value [0,1] of angle around the Y axis from X=-1.
+        // v: returned value [0,1] of angle from Y=-1 to Y=+1.
+        //     <1 0 0> yields <0.50 0.50>       <-1  0  0> yields <0.00 0.50>
+        //     <0 1 0> yields <0.50 1.00>       < 0 -1  0> yields <0.50 0.00>
+        //     <0 0 1> yields <0.25 0.50>       < 0  0 -1> yields <0.75 0.50>
+        let theta = f64::acos(-p.y);
+        let phi = f64::atan2(-p.z, p.x);
+
+        (phi / (2.0 * PI), theta / PI)
+    }
 }
 
 impl<'a> Hittable for Sphere<'a> {
@@ -66,13 +81,10 @@ impl<'a> Hittable for Sphere<'a> {
             }
         }
         let p = r.at(root);
-        Some(HitRecord::new(
-            r,
-            root,
-            p,
-            (p - current_centre) / self.radius,
-            &*self.mat,
-        ))
+        let normal = (p - current_centre) / self.radius;
+        let mut rec = HitRecord::new(r, root, p, normal, &*self.mat);
+        (rec.u, rec.v) = Self::get_sphere_uv(rec.normal);
+        Some(rec)
     }
 
     fn bounding_box(&self) -> AABB {
