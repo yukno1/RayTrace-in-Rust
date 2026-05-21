2f8fe86f2c703d2f9dddb145a162004766c39510
Yuk1no <121278340+yukno1@users.noreply.github.com>
2026-05-21 16:30:48 +0800

    Adjusting the Phase

--- src/texture/noise_tex.rs
+++ src/texture/noise_tex.rs
@@ -27,6 +27,6 @@ impl Default for NoiseTexture {
 
 impl Texture for NoiseTexture {
     fn value(&self, _u: f64, _v: f64, p: Point3) -> Color {
-        self.noise.turb(p, 7) * Color::new(1.0, 1.0, 1.0)
+        (1.0 + (self.scale * p.z + 10.0 * self.noise.turb(p, 7)).sin()) * Color::new(0.5, 0.5, 0.5)
     }
 }
