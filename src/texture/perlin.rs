ac6af197891ed22ca194bfdf2f992d0e1c8579a0
Yuk1no <121278340+yukno1@users.noreply.github.com>
2026-05-21 15:16:12 +0800

    trilinear interpolation

--- src/texture/perlin.rs
+++ src/texture/perlin.rs
@@ -25,12 +25,27 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: Point3) -> f64 {
-        // rust 不会像 c++ 那样通过补码将负数回绕到 [0，255]
-        let i = ((4.0 * p.x).floor() as isize & 255) as usize;
-        let j = ((4.0 * p.y).floor() as isize & 255) as usize;
-        let k = ((4.0 * p.z).floor() as isize & 255) as usize;
+        let u = p.x - (p.x).floor();
+        let v = p.y - (p.y).floor();
+        let w = p.z - (p.z).floor();
 
-        self.randfloat[self.perm_x[i] ^ self.perm_y[j] ^ self.perm_z[k]]
+        // rust 不会像 c++ 那样通过补码将负数回绕到 [0，255]
+        let i = p.x.floor() as isize;
+        let j = p.y.floor() as isize;
+        let k = p.z.floor() as isize;
+        let mut c = [[[0.0; 2]; 2]; 2];
+
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    c[di][dj][dk] = self.randfloat[self.perm_x[((i + di as isize) & 255) as usize]
+                        ^ self.perm_y[((j + dj as isize) & 255) as usize]
+                        ^ self.perm_z[((k + dk as isize) & 255) as usize]];
+                }
+            }
+        }
+
+        Self::trilinear_interp(c, u, v, w)
     }
 
     fn perlin_generate_perm(p: &mut [usize; POINT_COUNT]) {
@@ -47,6 +62,21 @@ impl Perlin {
             p[i] = p[target];
             p[target] = tmp;
         }
+    }
+
+    fn trilinear_interp(c: [[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+        let mut accum = 0.0;
+        for i in 0..2 {
+            for j in 0..2 {
+                for k in 0..2 {
+                    accum += (i as f64 * u + (1 - i) as f64 * (1.0 - u))
+                        * (j as f64 * v + (1 - j) as f64 * (1.0 - v))
+                        * (k as f64 * w + (1 - k) as f64 * (1.0 - w))
+                        * c[i][j][k];
+                }
+            }
+        }
+        accum
     }
 }
 
